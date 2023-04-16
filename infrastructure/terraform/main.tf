f00ae34fbecb3657573d43c456842d6d67aa7e57
Eduardo <eduardomoura.moura@gmail.com>
2023-04-16 11:35:51 -0300

    fix tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,9 +15,8 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  //credentials = file(var.credentials_file)
-  region = var.region
-  zone   = var.zone
+  region  = var.region
+  zone    = var.zone
 }
 
 module "project_ops" {
