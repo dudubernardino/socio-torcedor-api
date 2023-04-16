ffd2dab796c2a78ffcecd59f4ac5e27a8d549928
Eduardo <eduardomoura.moura@gmail.com>
2023-04-16 11:05:24 -0300

    fix tf version

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,14 +1,7 @@
 terraform {
-  required_version = ">= 0.13"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.5"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.5"
-    }
+    google      = ">= 3.43, < 5.0"
+    google-beta = ">= 3.43, < 5.0"
   }
 }
 
